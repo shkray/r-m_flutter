c90c327ef1c6fdfbdc466bf15c87605be423af54
shkray <cutaco.business@gmail.com>
2023-07-27 00:48:54 +1000

    create basic BottomNavigationBar and changed WelcomeWidget to stateful widget

--- lib/screens/welcomeScreen/welcome_screen.dart
+++ lib/screens/welcomeScreen/welcome_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'app_bar_widgets.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
 
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -13,9 +18,28 @@ class WelcomeScreen extends StatelessWidget {
           title: const AppBarTitleWidget(),
           actions: const [AppBarActionsWidget()],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: BottomNavigationBarItems.items,
+
+        ),
       ),
     );
   }
 }
 
-
+class BottomNavigationBarItems{
+  static final items = <BottomNavigationBarItem>[
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: "Персонажи"
+    ),
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.location_on),
+        label: "Локации"
+    ),
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.live_tv_outlined),
+        label: "Эпизоды"
+    )
+  ];
+}
